e4f88de5bc0c4fd1ac9ff47f8a6e4848d0e02085
Andrew <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 14:24:37 -0700

    First Commit

---
+++ booyah.sh
@@ -0,0 +1,7 @@
+echo -- Starting ImageCat Install --
+sleep 2
+echo -- Getting ImageCat --
+git clone https://github.com/chrismattmann/imagecat.git
+cd imagecat
+echo ===================== Beginning Setup ======================
+./booyah
