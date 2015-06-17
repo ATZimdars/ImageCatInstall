91295bf3ab22fd31b0842a8b72ded4f42d9ac137
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 15:09:48 -0700

    Update booyah.sh

--- booyah.sh
+++ booyah.sh
@@ -4,4 +4,5 @@ echo -- Getting ImageCat --
 git clone https://github.com/ATZimdars/imagecat.git
 cd imagecat
 echo ===================== Beginning Setup ======================
+chmod +x booyah.sh
 ./booyah.sh
